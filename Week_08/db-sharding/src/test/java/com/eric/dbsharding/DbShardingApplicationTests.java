b283f5dae6a214a86103ecb705e7464e22c17224
wdra233 <wangyawang666@gmail.com>
2020-12-10 01:29:33 -0800

    adding db-sharding init codes

---
+++ Week_08/db-sharding/src/test/java/com/eric/dbsharding/DbShardingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eric.dbsharding;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DbShardingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
